9b02c0947451556ce0ccbc00eb19929736d56ba2
jayann284 <annedroidd243@gmail.com>
2022-11-10 16:30:56 +0800

    activites and examples of while loops

---
+++ cpe007/while-loop/while2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    while (i <= 5)
+    {
+        cout << "Good morning! \n";
+        i++;
+    }
+
+    cout << "End of the loop body!";
+
+    return 0;
+}
